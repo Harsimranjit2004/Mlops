f63e1b3eaa103968b0cb8f1ce284e9a17de063d7
Harsimanji-singh <hsdosanjh81468@gmail.com>
2024-09-03 16:23:34 -0400

    initial commit

---
+++ mlartifacts/922170802804479150/b55ab9328b4343048f1a123a51e4620b/artifacts/iris-dt.py
@@ -0,0 +1,48 @@
+import pandas as pd 
+import mlflow.sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from sklearn.datasets import load_iris
+# Load the iris dataset
+
+mlflow.set_tracking_uri('http://127.0.0.1:5000')
+iris = load_iris()
+X = iris.data 
+y = iris.target 
+
+X_train, X_test, y_train, y_test = train_test_split(X, y , test_size=0.2 , random_state=42)
+
+max_depth = 15
+n_estimators = 10
+
+
+mlflow.set_experiment('iris-dt')
+
+with mlflow.start_run():
+    dt = DecisionTreeClassifier(max_depth=max_depth)
+    dt.fit(X_train, y_train)
+
+    y_pred = dt.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+
+    mlflow.log_metric('accuracy', accuracy)
+
+    mlflow.log_param('max_depth',max_depth)
+
+    print('accuracy', accuracy)
+
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm,annot=True, fmt='d',cmap='Blues', xticklabels=iris.target_names, yticklabels=iris.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('confusion matrix')
+
+    plt.savefig('confusion_matrix.png')
+    mlflow.log_artifact('confusion_matrix.png')
+    mlflow.log_artifact(__file__)
